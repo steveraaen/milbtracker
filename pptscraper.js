4a21ffd5232d5680a79b2e7607297a9ffd3cfc5b
Steveraaen <sraaen@gmail.com>
2019-05-07 20:27:22 -0400

    ..

--- pptscraper.js
+++ pptscraper.js
@@ -1,23 +1,95 @@
-
 const puppeteer = require('puppeteer');
+const fs = require('fs');
+const playerIDs = require('./playerIDs.json');
+
 
 (async () => {
-  const browser = await puppeteer.launch({ headless: false, slowMo: 250 });
+  const browser = await puppeteer.launch(/*{ headless: false, slowMo: 250 }*/);
   const page = await browser.newPage();
-  const TEAM_ROW_SELECTOR = 'tr.full_table'
   
-
   await page.goto('https://www.baseball-reference.com/leagues/MLB/2019-standard-batting.shtml', {waitUntil: 'networkidle2'});
 
+/*const data = await page.$$eval('#players_standard_batting> tbody > tr', trs => trs.map((tr) => {	
+					const dataNodeList = tr.querySelectorAll('td')
+					console.log(dataNodeList)
+		return tr.innerHTML;
+}))*/
+  const packages = await page.evaluate(() => {
+    var nodes = document.querySelectorAll('#players_standard_batting> tbody > tr');
+    var cells = [...nodes].map(el => el.innerText)
 
-const bodyHandle = await page.$('#players_standard_batting > tbody > tr');
+    return cells;
+  });
+const playerArr = []
+var playerObjArr = []
+for(let i = 0; i < packages.length; i++) {	
+	playerArr.push(packages[i].split(/\t/))
+	}
+	
+for(let i = 0; i < playerArr.length; i++) {
+	var playerObj={}
+	playerArr[i][1] = playerArr[i][1].replace(/\*/g , '').replace(/\#/g , '')
 
+	playerObj.playerName = playerArr[i][1];
+	playerObj.age = playerArr[i][2];
+	playerObj.team = playerArr[i][3];
+	playerObj.league = playerArr[i][4];
+	playerObj.G = playerArr[i][5];
+	playerObj.PA = playerArr[i][6];
+	playerObj.AB = playerArr[i][7];
+	playerObj.R = playerArr[i][8];
+	playerObj.H = playerArr[i][9];
+	playerObj.B2 = playerArr[i][10];
+	playerObj.B3 = playerArr[i][11];
+	playerObj.HR = playerArr[i][12];
+	playerObj.RBI = playerArr[i][13];
+	playerObj.SB = playerArr[i][14];
+	playerObj.CS = playerArr[i][15];
+	playerObj.BB = playerArr[i][16];
+	playerObj.SO = playerArr[i][17];
+	playerObj.BA = playerArr[i][18];
+	playerObj.OBP = playerArr[i][19];
+	playerObj.SLG = playerArr[i][20];
+	playerObj.OPS = playerArr[i][21];
+	playerObj.OPSP = playerArr[i][22];
+	playerObj.TB = playerArr[i][23];
+	playerObj.GDP = playerArr[i][24];
+	playerObj.HBP = playerArr[i][25];
+	playerObj.SH = playerArr[i][26];
+	playerObj.IBB = playerArr[i][27];
+	playerObj.POS= playerArr[i][28];
+	
 
-const html = await page.evaluate(el => el.innerHTML, bodyHandle);
+	playerObjArr.push(playerObj)
 
-console.log(html)
-  await browser.close();
+}
+
+	for(let j=0; j < playerIDs.length; j++) {
+		console.log(playerIDs[j].playerName === 'Glenn Sparkman')
+	}
+
+ await browser.close();
 })();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
